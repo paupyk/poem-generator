809b0fa5229232abe04c1f54e22add6e7b521813
paupyk <kayleapaup@yahoo.com>
2024-04-27 19:00:05 -0500

    Added basic form handling and typewriter plugin

---
+++ src/index.js
@@ -0,0 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "words words poem words",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
