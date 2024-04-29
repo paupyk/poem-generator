118a8fda411ada6f594ad7f07480ae4dba5500f4
paupyk <kayleapaup@yahoo.com>
2024-04-28 20:52:04 -0500

    Updated User Experience

--- src/index.js
+++ src/index.js
@@ -16,6 +16,9 @@ function generatePoem(event) {
   let context =
     "You are a contemporary poem expert and love to write short poems. Your mission is to generate a 4 line poem in html. Make sure to follow the user instructions below. Do not include a poem title. Sign the poem `SheCodes AI` inside a <strong> element at the end of the poem";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructionsInput.value}</div>`;
   axios.get(apiURL).then(displayPoem);
 }
 let poemFormElement = document.querySelector("#poem-generator-form");
